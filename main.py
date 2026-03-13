b366995a8833dad4cbeb5763390a0d8d7fc6a791
gustavoFalcao-dev <luizgf2019@gmail.com>
2026-03-12 21:14:46 -0300

    Criação do main.py para testes

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+from .env
+
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format (message))
+
+
+client = Client()
+client.run(env.bot_token)
